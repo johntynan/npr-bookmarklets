b54f20a393d0bf1cc84d09b0873fff94ea6a98df
jgtynan@gmail.com <jgtynan@gmail.com@229e3e12-6fc2-11a4-6b54-0a5ac12d678b>
2010-09-07 19:21:29 +0000

    Updates the script, based on the original code: "Note that we fade out and remove the wikiframe content if the user re-clicks the bookmarklet after it has loaded and if the user clicks on its background veil."

--- trunk/add_story_to_table.js
+++ trunk/add_story_to_table.js
@@ -121,23 +121,33 @@ function viewStoryTable(){
 	// s = story_table[1]
     // s = '10192781'
     // alert(story_id)
-    
-    if ((s != "") && (s != null)) {
-        $("body").append("\
-	<div id='wikiframe'>\
-		<div id='wikiframe_veil' style=''>\
-			<p id='wikiframe_content'>test<\p>\
-		</div>\
-		<style type='text/css'>\
-			#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
-			#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
-			#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,1); margin: -5px 0 0 -5px; }\
-		</style>\
-	</div>");
-        $("#wikiframe_veil").fadeIn(750);
-		$("#wikiframe_content").html(s);
 
-    }
+	if ($("#wikiframe").length == 0) {
+		if ((s != "") && (s != null)) {
+			$("body").append("\
+			<div id='wikiframe'>\
+				<div id='wikiframe_veil' style=''>\
+					<p id='wikiframe_content'>test<\p>\
+				</div>\
+				<style type='text/css'>\
+					#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+					#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
+					#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+				</style>\
+			</div>");
+			$("#wikiframe_veil").fadeIn(750);
+			$("#wikiframe_content").html(s);
+		}
+	} else {
+		$("#wikiframe_veil").fadeOut(750);
+		$("#wikiframe iframe").slideUp(500);
+		setTimeout("$('#wikiframe').remove()", 750);
+	}
+	$("#wikiframe_veil").click(function(event){
+		$("#wikiframe_veil").fadeOut(750);
+		$("#wikiframe iframe").slideUp(500);
+		setTimeout("$('#wikiframe').remove()", 750);
+	});
 }
 
 // alert(story_hostname);
